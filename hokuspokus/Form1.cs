87d60f9e8a4464887e5f4a68a5daed6a1872267b
Roman Veškrna <VeskrnaR.02@spst.eu>
2020-11-18 08:05:56 +0100

    součet

--- hokuspokus/Form1.cs
+++ hokuspokus/Form1.cs
@@ -16,5 +16,13 @@ namespace hokuspokus
         {
             InitializeComponent();
         }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(textBox1.Text);
+            int b = Convert.ToInt32(textBox2.Text);
+            int c = a + b;
+            label1.Text = c.ToString();
+        }
     }
 }
